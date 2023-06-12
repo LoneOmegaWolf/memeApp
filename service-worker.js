949869dced11951ef504b25db7621b83d5e9031f
Rick Snijders <97365268+LoneOmegaWolf@users.noreply.github.com>
2023-06-12 16:57:14 +0200

    d

--- service-worker.js
+++ service-worker.js
@@ -1,120 +1,47 @@
+// This is the "Offline page" service worker
 
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
-// This variable is intentionally declared and unused.
-// Add a comment for your linter if you want:
-// eslint-disable-next-line no-unused-vars
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
+const CACHE = "pwabuilder-page";
 
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = "offline";
-// Customize this with a different URL if needed.
-const OFFLINE_URL = "offline.html";
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html";
 
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        (async () => {
-            const cache = await caches.open(CACHE_NAME);
-            // Setting {cache: 'reload'} in the new request ensures that the
-            // response isn't fulfilled from the HTTP cache; i.e., it will be
-            // from the network.
-            await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
-        })()
-    );
-    // Force the waiting service worker to become the active service worker.
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        (async () => {
-            // Enable navigation preload if it's supported.
-            // See https://developers.google.com/web/updates/2017/02/navigation-preload
-            if ("navigationPreload" in self.registration) {
-                await self.registration.navigationPreload.enable();
-            }
-        })()
-    );
-
-    // Tell the active service worker to take control of the page immediately.
-    self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-    // Only call event.respondWith() if this is a navigation request
-    // for an HTML page.
-    if (event.request.mode === "navigate") {
-        event.respondWith(
-            (async () => {
-                try {
-                    // First, try to use the navigation preload response if it's
-                    // supported.
-                    const preloadResponse = await event.preloadResponse;
-                    if (preloadResponse) {
-                        return preloadResponse;
-                    }
-
-                    // Always try the network first.
-                    const networkResponse = await fetch(event.request);
-                    return networkResponse;
-                } catch (error) {
-                    // catch is only triggered if an exception is thrown, which is
-                    // likely due to a network error.
-                    // If fetch() returns a valid HTTP response with a response code in
-                    // the 4xx or 5xx range, the catch() will NOT be called.
-                    console.log("Fetch failed; returning offline page instead.", error);
-
-                    const cache = await caches.open(CACHE_NAME);
-                    const cachedResponse = await cache.match(OFFLINE_URL);
-                    return cachedResponse;
-                }
-            })()
-        );
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
     }
-
-    // If our if() condition is false, then this fetch handler won't
-    // intercept the request. If there are any other fetch handlers
-    // registered, they will get a chance to call event.respondWith().
-    // If no fetch handlers call event.respondWith(), the request
-    // will be handled by the browser as if there were no service
-    // worker involvement.
 });
 
+self.addEventListener('install', async (event) => {
+    event.waitUntil(
+        caches.open(CACHE)
+            .then((cache) => cache.add(offlineFallbackPage))
+    );
+});
 
-// const CACHE_NAME = 'cool-cache';
+if (workbox.navigationPreload.isSupported()) {
+    workbox.navigationPreload.enable();
+}
 
-// // Add whichever assets you want to pre-cache here:
-// const PRECACHE_ASSETS = [
-//     '/assets/',
-//     '/src/'
-// ]
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async () => {
+            try {
+                const preloadResp = await event.preloadResponse;
 
-// // Listener for the install event - pre-caches our assets list on service worker install.
-// self.addEventListener('install', event => {
-//     event.waitUntil((async () => {
-//         const cache = await caches.open(CACHE_NAME);
-//         cache.addAll(PRECACHE_ASSETS);
-//     })());
-// });
+                if (preloadResp) {
+                    return preloadResp;
+                }
 
-// self.addEventListener('activate', event => {
-//     event.waitUntil(clients.claim());
-// });
+                const networkResp = await fetch(event.request);
+                return networkResp;
+            } catch (error) {
 
-// self.addEventListener('fetch', event => {
-//     event.respondWith(async () => {
-//         const cache = await caches.open(CACHE_NAME);
-
-//         // match the request to our cache
-//         const cachedResponse = await cache.match(event.request);
-
-//         // check if we got a valid response
-//         if (cachedResponse !== undefined) {
-//             // Cache hit, return the resource
-//             return cachedResponse;
-//         } else {
-//             // Otherwise, go to the network
-//             return fetch(event.request)
-//         };
-//     });
-// });
+                const cache = await caches.open(CACHE);
+                const cachedResp = await cache.match(offlineFallbackPage);
+                return cachedResp;
+            }
+        })());
+    }
+});
